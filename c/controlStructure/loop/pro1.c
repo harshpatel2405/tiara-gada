34a22371a1f849cc5cd075b731523ccc2f977ad1
harshpatel2405 <harshpatel4504@gmail.com>
2026-08-08 18:01:19 +0530

    loop

---
+++ c/controlStructure/loop/pro1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{
+    // * print Hello World 10 times
+    int i;
+
+    // * for loop
+    // for (i = 1; i <= 10; i++)
+    // {
+    //     printf("Hello World\n");
+    // }
+
+    // * while loop
+    // i = 1;
+    // while (i <= 10)
+    // {
+    //     printf("Hello World\n");
+    //     i++;
+    // }
+
+    // * do .. while loop
+    // i = 1;
+    // do
+    // {
+    //     printf("Hello World\n");
+    //     i++;
+    // } while (i <= 10);
+
+    // * Print 1 to 10
+    // for (i = 1; i <= 10; i++)
+    // {
+    //     printf("%d\t", i);
+    // }
+
+    // * print N to 1
+    // int n;
+    // printf("Enter value for n : ");
+    // scanf("%d", &n);
+    // for (i = n; i >= 1; i--)
+    // {
+    //     printf("%d\t", i);
+    // }
+
+    // * Sum of 1 to 500
+    // int sum = 0;
+    // for (i = 1; i <= 500; i++)
+    // {
+    //     // printf("Iteration %d -> Sum = %d + %d = ", i, sum, i);
+    //     sum = sum + i;
+    //     // printf("%d\n", sum);
+    // }
+    // printf("Final Sum : %d", sum);
+    return 0;
+}
